7dccd8c267975807aba1d113f1b07f6e9611bd0f
neverovskij <m.neverovskij@cft.ru>
2023-02-11 19:19:04 +0700

    some fixes

--- app/src/main/java/ru/neverovskij/notes/MainActivity.kt
+++ app/src/main/java/ru/neverovskij/notes/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             NotesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
